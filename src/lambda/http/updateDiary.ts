16df3aef293f4c22b0c0a0be5b990ee590befdff
mahmoudselmy06@gmail.com <mahmoud_sam_06>
2020-05-02 16:21:26 +0200

    + update diary

---
+++ src/lambda/http/updateDiary.ts
@@ -0,0 +1,22 @@
+import 'source-map-support/register'
+
+import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+
+import { DiaryRequest } from '../../requests/DiaryRequest'
+import { updateDiary } from '../../buisnessLogic/diary'
+import {getUserId} from '../utils'
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const diaryId = event.pathParameters.todoId
+  const updatedDiaryRequest: DiaryRequest = JSON.parse(event.body)
+
+  const userId = getUserId(event);
+
+  await updateDiary(diaryId, userId, updatedDiaryRequest)
+
+  return {
+    statusCode: 200,
+    headers: {'Access-Control-Allow-Origin': '*'},
+    body: JSON.stringify({})
+  }
+}
